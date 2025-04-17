95e75792e25fec383e8a9431c626f73198153da4
atishe <you@example.com>
2025-04-17 21:38:18 +0530

    distfix

---
+++ dist/assets/AdminProtected-c224ef7d.js
@@ -0,0 +1,1 @@
+import{x as s,j as a,N as r}from"./index-a75d0303.js";const i=({compo:t})=>{const{admin:e}=s(o=>o.auth);return e?t:a.jsx(r,{to:"/login"})};export{i as default};
---
+++ dist/assets/ProfessinalProtected-23198afb.js
@@ -0,0 +1,1 @@
+import{x as s,j as r,N as a}from"./index-a75d0303.js";const i=({compo:t})=>{const{profe:e}=s(o=>o.auth);return e?t:r.jsx(a,{to:"/login"})};export{i as default};
